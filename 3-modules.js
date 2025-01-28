4138eddcfcff37ee9dc0a592e8fb201e2d466ec0
Khalil Hafz <khalilhafz@gmail.com>
2025-01-29 00:20:01 +0200

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,16 @@
+// Module 
+
+
+const names = require('./4-names');
+const sayHi = require('./5-utils');
+const data = require('./6-alternative-flavor');
+console.log(data);
+require('./7-mind');
+
+
+sayHi('Khalil');
+sayHi(names.john);
+sayHi(names.peter);
+
+
+
