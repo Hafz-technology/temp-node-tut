5c9440612019ac65bc8f75ba232557b8c0c1f5e7
Khalil Hafz <khalilhafz@gmail.com>
2025-01-30 21:10:21 +0200

    update

--- app.js
+++ app.js
@@ -13,17 +13,34 @@
 // npm init (step by step, press enter to skip)
 // npm init -y (everything default)
 
-const _ = require('lodash');
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
-console.log(newItems);
-console.log('Hello people');
+// const _ = require('lodash');
+// const items = [1, [2, [3, [4]]]];
+// const newItems = _.flattenDeep(items);
+// console.log(newItems);
+// console.log('Hello world');
+
+// const {readFile} = require('fs');
+// console.log('started a first task');
+// readFile('./content/first.txt', 'utf8', (err, result) => {
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+//     console.log(result);
+//     console.log('completed first task');
+// });
+
+// console.log('starting next task');
 
 
+// started operating system process
+console.log('first');
+setTimeout(() => {
+    console.log('second');
+}, 0 );
 
-
-
-
+console.log('third');
+// completed and exited operating system process
 
 
 
