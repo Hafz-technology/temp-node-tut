e09a598cef11324c3332e0c4f04c82954d54c995
Khalil Hafz <khalilhafz@gmail.com>
2025-01-30 23:31:01 +0200

    update async promises

--- app.js
+++ app.js
@@ -34,30 +34,106 @@
 
 
 // started operating system process
-console.log('first');
-setTimeout(() => {
-    console.log('second');
-}, 0 );
+// console.log('first');
+// setTimeout(() => {
+//     console.log('second');
+// }, 0 );
 
-console.log('third');
-// completed and exited operating system process
+// console.log('third');
+// // completed and exited operating system process
+
+// setInterval(() => {
+//     console.log('hello world');
+// }, 2000);
+// console.log('I will run first');
+// // process stays alive unless
+// // Kill Process CONTROL + C
+// // unexpected error
+
+// const http = require('http');
+// const server = http.createServer((req, res) => {
+//     console.log('request event');
+//     res.end('Hello World');
+// });
+
+// server.listen(5000, () => {
+//     console.log('Server listening on port : 5000....');
+// });
 
 
 
 
 
+// const http = require('http');
+
+
+// const server = http.createServer((req, res) => {
+//     if (req.url === '/') {
+//         res.end('Home page');
+//     }
+//     if (req.url === '/about') {
+//         for (let i = 0; i < 100; i++) {
+//             for (let j = 0; j < 100; j++) {
+//                 console.log(`${i} ${j}`);
+//             }
+//         }
+//         res.end('about page');
+//     }
+//     res.end('Error page');
+// });
+
+
+// server.listen(5000, () => {
+//     console.log('Server listening on port : 5000....');
+// });
 
 
 
+const {readFile, writeFile} = require('fs').promises;
+// const util = require('util');
+
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
+
+
+const start = async () => {
+    try {
+        const first = await readFile('./content/first.txt', 'utf8');
+        const second = await readFile('./content/second.txt', 'utf8');
+        await writeFile('./content/result-mind-grenade.txt', `This is the result: ${first}, ${second}`, {flag: 'a'});
+        console.log(first, second);
+        
+    } catch (error) {
+        console.log(error);
+    }
+    
+    
+    
+    
+};
+
+start();
 
 
 
+// const getText = (path) => {
+//     return new Promise((resolve, reject) => {
+//         readFile(path, 'utf8', (err, data) => {
+//             if (err) {
+//                 reject(err);
+//             } 
+//             else {
+//              resolve(data);  
+//              }
+//         });
+//     })
+// };
 
-
-
-
-
-
+// getText('./content/first.txt').then((result) => {
+//     console.log(result);
+// }).catch((err) => {
+//     console.log(err);
+// });
 
 
 
